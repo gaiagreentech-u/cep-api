c3a03ab7b1a42f940da25e989b1eed31572ffa07
Marcelo <marcelo.chiarella@gmail.com>
2024-03-15 19:38:34 -0300

    get cep by cep now returns 200 or 404 for found/not found.

--- src/server.ts
+++ src/server.ts
@@ -160,9 +160,9 @@ app.get('/cep', async (request, reply) => {
                     }
                     
                     if (found_cep){
-                        return {'message': 'Estamos disponíveis em sua cidade: ' + found_regiao}
+                        return reply.status(200).send({'message': 'Estamos disponíveis em sua cidade: ' + found_regiao})
                     } else {
-                        return {'message': 'Ainda não chegamos na sua cidade. Estamos trabalhando para levar a GAIA para todo o Brasil. Você pode nos ajudar a acelerar nossa revolução, nos indicando para sua marca de eletroeletrônicos favorita.'}
+                        return reply.status(404).send({'message': 'Ainda não chegamos na sua cidade. Estamos trabalhando para levar a GAIA para todo o Brasil. Você pode nos ajudar a acelerar nossa revolução, nos indicando para sua marca de eletroeletrônicos favorita.'})
                     }
                 }
             } 
